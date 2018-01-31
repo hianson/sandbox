aab93178eeae9b2639c65131ee00cabaa8bb4638
Anson <leungans@gmail.com>
2018-01-31 02:18:47 -0800

    add socket.io

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
+// server for socket.io:
+var serv = require('http').Server(app);
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT)
+serv.listen(PORT)
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 app.use('/public', express.static(__dirname + '/public'));
+
+
+var io = require('socket.io')(serv, {});
+
+io.sockets.on('connection', function(socket) {
+  socket.id = Math.random();
+  console.log('Connection made:', socket.id)
+
+  // while player connected, listen for these events:
+  socket.on('keyPress', function(data) {
+    if (data.inputId === 'left') {
+      player.pressingLeft = data.state;
+      // change player lookDirection here
+    } else if (data.inputId === 'right') {
+      player.pressingRight = data.state;
+    } else if (data.inputId === 'up') {
+      player.pressingUp = data.state;
+    } else if (data.inputId === 'down') {
+      player.pressingDown = data.state;
+    }
+  });
+
+  socket.on('disconnect', function() {
+    delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+    console.log('Connection ended.')
+  })
+
+});
